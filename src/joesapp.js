18cf8b8e07121e28f3fe22dbec276a20a1195531
Joe Duncko <dotmaners@gmail.com>
2014-11-21 23:16:41 -0500

    Cleanup, stats work

--- src/joesapp.js
+++ src/joesapp.js
@@ -23,10 +23,11 @@ var error = new UI.Card({
 main.show();
 
 main.on('click', 'up', function(e) {
-    var card = new UI.Card();
-    card.title('Stats');
-    card.body('Health, money, wall inventory,  kills, time alive, time until next wave');
-    card.show();
+    var stats = new UI.Card();
+    stats.title('Stats');
+    stats.subtitle('Health: '+totalHealth.toString()+'/20');
+    stats.body('Coins: '+totalCoins+', wall inventory: '+wallInventory+',  kills, time alive, time until next wave');
+    stats.show();
 });
 
 main.on('click', 'select', function(e) {
@@ -98,59 +99,11 @@ main.on('click', 'select', function(e) {
   menu.show();
 });
 
-/*wind.on('click', 'select', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      // Items are instant use upon purchase
-      items: [{
-        title: 'Buy Health (2)',
-        subtitle: 'Heals for 1 hp'
-      }, {
-        title: 'Buy Wall (3)',
-        subtitle: 'Takes the next hit'
-      }, {
-        title: 'Buy Nuke (4)',
-        subtitle: 'Clears every tile'
-      }]
-    }]
-  });
-  
-  menu.on('select', function(e) {
-    // If Health was purchased
-    if (e.sectionIndex === 0 && e.itemIndex === 0) {
-      if (totalHealth < 20 && totalCoins >= 2) {
-        totalHealth = ++totalHealth;
-        totalCoins = totalCoins - 2;
-        console.log("Health was purchased");
-        console.log(totalHealth);
-        console.log(totalCoins);
-      }
-      else {
-        console.log("Not enough coins");
-      }
-    }
-    
-    // If Wall was purchased
-    if (e.sectionIndex === 0 && e.itemIndex === 1) {
-      
-      totalCoins = totalCoins - 3;
-      console.log("Wall was purchased");
-      console.log(totalCoins);
-    }
-    
-    // If Nuke was purchased
-    if (e.sectionIndex === 0 && e.itemIndex === 2) {
-      
-      totalCoins = totalCoins - 3;
-      console.log("Nuke was purchased");
-    }
-  });
-  menu.show();
-});*/
+
 
 main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('How to play');
-  card.body('Every 10 minutes enemies get closer to your base. Fend them off by purchasing items from the shop. ');
-  card.show();
+  var howto = new UI.Card();
+  howto.title('How to play');
+  howto.body('Every 10 minutes enemies get closer to your base. Fend them off by purchasing items from the shop. ');
+  howto.show();
 });
